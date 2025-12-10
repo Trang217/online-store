1c877b14f5aeba0a226f6235ecd4e635ba23618b
Trang <76183398+Trang217@users.noreply.github.com>
2025-12-10 15:13:48 +0100

    add products page

--- components/products/ProductsContainer.tsx
+++ components/products/ProductsContainer.tsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import { fetchAllProducts } from "@/utils/action";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { LayoutGrid, List } from "lucide-react";
+import { Separator } from "../ui/Separator";
+import ProductsGrid from "./ProductsGrid";
+import ProductsList from "./ProductsList";
 
-function ProductsContainer() {
+async function ProductsContainer({layout, search}: {layout:string; search:string}) {
+  const products = await fetchAllProducts();
+  const totalProducts = products.length;
+  const searchTerm = search ? `&search=${search}` : "";
+  
   return (
-    <div>ProductsContainer</div>
+    <>
+      <section>
+        <div className="flex justify-between items-center">
+          <h4 className="font-medium text-lg">{totalProducts} product{totalProducts > 1 && "s"}</h4>
+          <div className="flex gap-x-4">
+            <Button
+              variant={layout === "grid" ? "default" : "ghost"}
+              size="icon"
+              asChild>
+                <Link href={`/products?layout=grid${searchTerm}`}>
+                  <LayoutGrid />
+                </Link>
+            </Button>
+
+            <Button
+              variant={layout === "list" ? "default" : "ghost"}
+              size="icon"
+              asChild>
+                <Link href={`/products?layout=list${searchTerm}`}>
+                  <List />
+                </Link>
+            </Button>
+          </div>
+        </div>
+        <Separator className="mt-4" />
+      </section>
+
+      <div>
+        {
+          totalProducts === 0 ? (
+            <h5 className="text-2xl mt-16">Sorry, no products matched your search...</h5>
+          ) : layout === "grid" ? (
+            <ProductsGrid products={products} />
+          ) : (
+            <ProductsList products={products} />
+          )}
+      </div>
+    </>
   )
 }
 
